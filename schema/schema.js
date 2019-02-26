f984bf378cc7ce0b60869cf7123e6a8219b1c05f
asitkdash <asitdash.csu.fullerton.edu>
2019-02-26 10:57:10 -0800

    added company in schema and set a relation with user. added nodemon to watch

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,12 @@
 const graphql = require("graphql");
-const _ = require("lodash");
+const axios = require("axios");
 
 const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql;
 
-const users = [
-  { id: "23", firstName: "Bill", age: 20 },
-  { id: "47", firstName: "Samantha", age: 21 }
-];
+// const users = [
+//   { id: "23", firstName: "Bill", age: 20 },
+//   { id: "47", firstName: "Samantha", age: 21 }
+// ];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -24,7 +24,10 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        //        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
       }
     }
   }
