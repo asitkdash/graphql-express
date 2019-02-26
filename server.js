3126494670a7560bc9512bce0f5481522ede442b
asitkdash <asitdash.csu.fullerton.edu>
2019-02-26 10:23:46 -0800

    User schema added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
-
+const schema = require("./schema/schema");
 const app = express();
 
 app.use(
   "/graphql",
   expressGraphQL({
+    schema: schema,
     graphiql: true
   })
 );
